0edcd42493d8f81f94c8b835cd772a9e0b20f97b
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-22 07:17:11 +0300

    Made fragment

--- app/src/main/java/com/example/fragmentsapp/Fragment1.kt
+++ app/src/main/java/com/example/fragmentsapp/Fragment1.kt
@@ -1,14 +1,23 @@
 package com.example.fragmentsapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 
 class Fragment1 : Fragment() {
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Toast.makeText(context, "onAttach() is called", Toast.LENGTH_SHORT).show()
+    }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
